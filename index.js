4abf88bcd63a98bfcac130364c20db4ec6a3c42c
Andrew Parnell-Smith <andrewparnellsmith@gmail.com>
2018-06-24 03:05:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return "Happy Independence Day, you!"
 }
-function holidayCountdown(holiday, days)
+function holidayCountdown(holiday, days) {
+  return ""
+}
